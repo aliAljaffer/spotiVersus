f8d5e89684bb8f13d44ca88dd03487ef58741d51
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2024-09-08 12:36:06 +0300

    Clarification

--- src/hooks/supabase.tsx
+++ src/hooks/supabase.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-
+// This is a read-only API key and can only access the specific DB for this project
 const supabaseUrl = "https://hseaihljqoilgnsuanse.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZWFpaGxqcW9pbGduc3VhbnNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwOTc5MzcsImV4cCI6MjA0MDY3MzkzN30.yBz9_o35KNbm0xblNeFI_q_w4uthsBom4UHMfA8d8Pc";
